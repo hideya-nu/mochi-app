7d29efdb0e6b2ca565b67878cac2a2256da61ea1
鶴飼真弓 <>
2023-01-20 01:11:54 +0900

    no message

--- tsurukai/search.js
+++ tsurukai/search.js
@@ -1,7 +1,4 @@
-// use fetch to retrieve the products and pass them to init
-// report any errors that occur in the fetch operation
-// once the products have been successfully loaded and formatted as a JSON object
-// using response.json(), run the initialize() function
+
 fetch('products.json')
   .then( response => {
     if (!response.ok) {
@@ -20,29 +17,20 @@ function initialize(products) {
   const searchBtn = document.querySelector('button');
   const main = document.querySelector('main');
 
-  // keep a record of what the last category and search term entered were
-  let lastCategory = category.value;
-  // no search has been made yet
-  let lastSearch = '';
 
-  // these contain the results of filtering by category, and search term
-  // finalGroup will contain the products that need to be displayed after
-  // the searching has been done. Each will be an array containing objects.
-  // Each object will represent a product
+
+  // 結果の表示
   let categoryGroup;
   let finalGroup;
 
-  // To start with, set finalGroup to equal the entire products database
-  // then run updateDisplay(), so ALL products are displayed initially.
+  
   finalGroup = products;
   updateDisplay();
 
-  // Set both to equal an empty array, in time for searches to be run
-  categoryGroup = [];
+  // 検索の実行に間に合うように、両方を空の配列に設定します
   finalGroup = [];
 
-  // when the search button is clicked, invoke selectCategory() to start
-  // a search running to select the category of products we want to display
+  //検索ボタンがクリックされると、selectCategory() を呼び出して開始します
   searchBtn.addEventListener('click', selectCategory);
 
   function selectCategory(e) {
@@ -54,39 +42,9 @@ function initialize(products) {
     categoryGroup = [];
     finalGroup = [];
 
-    // if the category and search term are the same as they were the last time a
-    // search was run, the results will be the same, so there is no point running
-    // it again — just return out of the function
-    if (category.value === lastCategory && searchTerm.value.trim() === lastSearch) {
-      return;
-    } else {
-      // update the record of last category and search term
-      lastCategory = category.value;
-      lastSearch = searchTerm.value.trim();
-      // In this case we want to select all products, then filter them by the search
-      // term, so we just set categoryGroup to the entire JSON object, then run selectProducts()
-      if (category.value === 'All') {
-        categoryGroup = products;
-        selectProducts();
-      // If a specific category is chosen, we need to filter out the products not in that
-      // category, then put the remaining products inside categoryGroup, before running
-      // selectProducts()
-      } else {
-        // the values in the <option> elements are uppercase, whereas the categories
-        // store in the JSON (under "type") are lowercase. We therefore need to convert
-        // to lower case before we do a comparison
-        const lowerCaseType = category.value.toLowerCase();
-        // Filter categoryGroup to contain only products whose type includes the category
-        categoryGroup = products.filter( product => product.type === lowerCaseType );
 
-        // Run selectProducts() after the filtering has been done
-        selectProducts();
-      }
-    }
-  }
-
-  // selectProducts() Takes the group of products selected by selectCategory(), and further
-  // filters them by the tiered search term (if one has been entered)
+    // selectCategory() によって選択された製品のグループを取得し、さらに
+    // 階層化された検索語でそれらをフィルタリングします (入力されている場合)
   function selectProducts() {
     // If no search term has been entered, just make the finalGroup array equal to the categoryGroup
     // array — we don't want to filter the products further.
