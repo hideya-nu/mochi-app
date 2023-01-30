979d976119ba71f9ffdc7d0f4055c608a0d9d42f
hideya <fidea@gmail.com>
2023-01-30 15:27:11 +0900

    nodejs commit

--- node_test.js
+++ node_test.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const PDFImage = require("pdf-image").PDFImage;
+const pdfImage = new PDFImage("/images/企画書.pdf");
 const { Client } = require("@notionhq/client");
 
 const hostname = '127.0.0.1';
@@ -11,10 +13,15 @@ const server = http.createServer(RouteSetting);
 const indexPage = fs.readFileSync('./html/index.html', 'UTF-8');
 const textbookPage = fs.readFileSync('./html/textbook.html', 'UTF-8');
 const exhibitPage = fs.readFileSync('./html/exhibit.html', 'UTF-8');
+const informationPage = fs.readFileSync('./html/information.html', 'UTF-8');
 
 const mainCss = fs.readFileSync('./css/main.css', 'UTF-8');
 const indexJs = fs.readFileSync('./js/index.js', 'UTF-8');
 const textbookJs = fs.readFileSync('./js/textbook.js', 'UTF-8');
+
+const logoImg = fs.readFileSync('./images/ReNUT.png');
+const kikakuImg = fs.readFileSync('./images/企画書.pdf','UTF-8');
+
 
 
 server.listen(port,hostname, () => {
@@ -44,7 +51,14 @@ function RouteSetting(req, res) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(exhibitPage);
         res.end();
-        break;  
+        break;
+      
+      case '/html/information.html':
+        //'textbook.html'にアクセスした時の処理
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(informationPage);
+        res.end();
+        break;
 
       case '/css/main.css':
       // cssファイルにアクセスした時の処理
@@ -66,6 +80,20 @@ function RouteSetting(req, res) {
         res.write(textbookJs);
         res.end();
         break;
+      
+      case '/images/ReNUT.png':
+        // imagesファイルにアクセスした時の処理
+        res.writeHead(200, {'Content-Type': 'image/png'});
+        res.write(logoImg);
+        res.end();
+        break;
+
+      case '/images/企画書.pdf':
+        // imagesファイルにアクセスした時の処理
+        res.writeHead(200, {'Content-Type': 'image/pdf'});
+        res.write(kikakuImg);
+        res.end();
+        break;
 
       default:
         //ダメだった時の処理
@@ -74,3 +102,4 @@ function RouteSetting(req, res) {
         break;
     }
   }
+
